42d064a1cd36e83447df550ce1c5bc32fe705b8c
titarenko <arseniytitarenko93@yandex.ru>
2025-02-18 20:32:09 +0300

    quick fix

---
+++ diploma_loader_service/constants.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
+DIPLOMA_LOADER_TOPIC = "user.diplomas.upload"
+REDIS_TOPICS = [DIPLOMA_LOADER_TOPIC]
+
+API_URL = os.getenv("API_URL")
+API_PORT = os.getenv("API_PORT")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
